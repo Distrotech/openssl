f59e8e2cd0d9d375aea4c0e781f57f266e95a4db
Richard Levitte <levitte@openssl.org>
2002-10-11 22:25:57 +0000

    Latest changes from the main trunk.

--- engines/vendor_defns/hwcryptohook.h
+++ engines/vendor_defns/hwcryptohook.h
@@ -65,7 +65,7 @@
  * please contact nCipher.
  *
  *
- * $Id: hwcryptohook.h,v 1.1 2002/10/11 17:10:59 levitte Exp $
+ * $Id: hwcryptohook.h,v 1.1.4.1 2002/10/11 22:19:37 levitte Exp $
  */
 
 #ifndef HWCRYPTOHOOK_H
